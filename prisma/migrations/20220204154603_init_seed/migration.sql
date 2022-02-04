0451bf83de3dd082007abbdbd963b1ede5c06da6
bravint <bravint@gmail.com>
2022-02-04 16:31:33 +0000

    requirement 3 completed

---
+++ prisma/migrations/20220204154603_init_seed/migration.sql
@@ -0,0 +1,109 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "username" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "pictureUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "imgUrl" TEXT NOT NULL,
+    "publishedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "parentId" INTEGER,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "postId" INTEGER NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CategoriesonPosts" (
+    "postId" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "CategoriesonPosts_pkey" PRIMARY KEY ("postId","categoryId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_title_key" ON "Post"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CategoriesonPosts" ADD CONSTRAINT "CategoriesonPosts_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CategoriesonPosts" ADD CONSTRAINT "CategoriesonPosts_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,14 +2,120 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const seed = async () => {
-    await prisma.user.create({})
-}
+    const user = await createUser();
+    await createProfile(user);
+    const categories = await createCategory();
+    const posts = await createPost(categories, user);
+    await createComment(user, posts)
+};
+
+const createUser = async () => {
+    const createdUser = await prisma.user.create({
+        data: {
+            username: 'Brave',
+            email: 'brave@mail.com',
+            password: 'niceTry',
+        },
+    });
+    console.log(`createdUser`, createdUser);
+    return createdUser;
+};
+
+const createProfile = async (user) => {
+    const createdProfile = await prisma.profile.create({
+        data: {
+            firstName: 'Bravin',
+            lastName: 'Thillainathan',
+            age: 34,
+            pictureUrl: 'http://yourUrlHere/picture.png',
+            user: {
+                connect: {
+                    id: user.id,
+                },
+            },
+        },
+    });
+    console.log(`createdProfile`, createdProfile);
+    return createdProfile;
+};
+
+const createCategory = async () => {
+    console.log(`creating category`)
+    const categoriesToCreate = ['catA', 'catB', 'catC', 'catD', 'catE'];
+    console.log(`categoriesToCreate`, categoriesToCreate)
+    let categories = [];
+    for (let i = 0; i < categoriesToCreate.length; i++) {
+        console.log(categoriesToCreate[i])
+        const createdCategory = await prisma.category.create({
+            data: {
+                name: categoriesToCreate[i],
+            },
+        });
+        categories.push(createdCategory);
+    }
+    console.log(`createdCategories`, categories);
+    return categories;
+};
+
+const createPost = async (categories, user) => {
+    const postsToCreate = ['postA', 'postB', 'postC', 'postD', 'postE'];
+    let posts = [];
+    for (let i = 0; i < postsToCreate.length; i++) {
+        const createdPost = await prisma.post.create({
+            data: {
+                title: postsToCreate[i],
+                content: postsToCreate[i],
+                imgUrl: 'http://yourUrlHere/picture.png',
+                user: {
+                    connect: {
+                        id: user.id,
+                    },
+                },
+                categories: {
+                    create: [
+                        {
+                            category: {
+                                connect: {
+                                    id: categories[i].id,
+                                },
+                            },
+                        },
+                    ],
+                },
+            },
+        });
+        console.log(createdPost)
+        posts.push(createdPost);
+    }
+    console.log(`createdPosts`, posts);
+    return posts;
+};
+
+const createComment = async (user, posts) => {
+    const createdComment = await prisma.comment.create({
+        data: {
+            content: `hello world, I'm replying to a post`,
+            user: {
+                connect: {
+                    id: user.id
+                }
+            },
+            post: {
+                connect: {
+                    id: posts[0].id
+                }
+            }
+        }
+    })
+    console.log(`createdComment`, createdComment);
+    return createdComment;
+};
 
 seed()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
