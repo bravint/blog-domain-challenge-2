0cada2c199f74d5342ef0fe702cf18b9f1c5d40b
bravint <bravint@gmail.com>
2022-02-07 10:19:39 +0000

    requirement 5 completed

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,28 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 const createUser = async (req, res) => {
-    console.log(req.body, req.params)
-    // createdUser = await prisma.user.create({
-
-    // })
-    return res.status(200). res.json('route OK')
-}
+    console.log(req.body, req.params);
+    const createdUser = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            profile: {
+                create: {
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    age: req.body.age,
+                    pictureUrl: req.body.pictureUrl,
+                },
+            },
+        },
+        include: {profile: true}
+    });
+    return res.json(createdUser);
+};
 
 module.exports = {
-    createUser
-}
+    createUser,
+};
